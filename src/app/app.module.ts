fe61cf91f98ad2fbe12777785aa780eee0f13023
apinj565 <apinya@odds.team>
2020-06-18 21:13:10 +0700

    Create component invester detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchClientComponent } from './search-client/search-client.component';
 import { HttpClientModule } from '@angular/common/http';
+import { InvesterDetailComponent } from './invester-detail/invester-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchClientComponent
+    SearchClientComponent,
+    InvesterDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/invester-detail/invester-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invester-detail',
+  templateUrl: './invester-detail.component.html',
+  styleUrls: ['./invester-detail.component.scss']
+})
+export class InvesterDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
