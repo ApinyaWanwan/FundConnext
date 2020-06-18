1bf2f3758eec93ef673dde47b26aa680de5bde37
apinj565 <apinya@odds.team>
2020-06-18 17:12:27 +0700

    Create service client

---
+++ src/app/services/client.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from 'selenium-webdriver/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+
+  constructor(private http: HttpClient) { }
+
+  getClients() {
+
+  }
+}
